98e07c596d6257a3c6189826a180ff522985fe51
Rasmus Wigand <liberating11@protonmail.com>
2021-09-11 06:41:54 +0700

    Listen to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ async fn main() -> Result<()> {
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await?;
 
